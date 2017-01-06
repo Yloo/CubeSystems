f30a4449e3a98fadea7fa11f14e14b1be4a1bcd2
Oleg <falsra@gmail.com>
2017-01-06 12:01:03 +0200

    Update 100.php

--- 100.php
+++ 100.php
@@ -1,4 +1,9 @@
 <?php
+/*
+  It is safe to use % operator while dealing with integers in range 1-100.
+  I would offer another solution for negative and large numbers.
+  There is a short code below but it could be written more 'readable' if needed.
+*/
   foreach (range(1,100) as $n) {	
     echo $n . ' ' . ( !($n%3)  ? 'CUBE' : '' ) . ( !($n%5) ? 'Systems' : '' ) . '<br>';
   }
